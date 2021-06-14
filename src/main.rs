8dcfcf3c8373e8f426db5817105958aa1623077e
Joshua Leger <legermjoshua@gmail.com>
2021-06-14 08:16:29 -0700

    refactored main file for readability

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,53 @@
 use prettify;
+use prettify::options::Options;
 extern crate clap;
-use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg};
+use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg, ArgMatches};
 
 fn main() {
-    let matches = App::new(crate_name!())
+    let matches = get_matches();
+
+    let input = build_input(&matches);
+    let options = build_options(&matches);
+
+    let result = prettify::prettify(&input, &options);
+
+    if result.is_empty() {
+        println!("Input was not in any recognized format.");
+        println!("{}", input);
+    } else {
+        println!("{}", result);
+    }
+}
+
+//TODO: Add ability to get input from file
+//TODO: Add ability to get input from stdin
+fn build_input(matches: &ArgMatches) -> String {
+    String::from(matches.value_of("INPUT").unwrap())
+}
+
+fn build_options(matches: &ArgMatches) -> Options {
+    Options {
+        indent_size: get_indent(matches),
+        verbose: matches.occurrences_of("verbose") > 0,
+    }
+}
+
+fn get_indent(matches: &ArgMatches) -> u8 {
+    let indent_str = matches.value_of("indent").unwrap();
+    match indent_str.parse::<u8>() {
+        Ok(i) => i,
+        Err(_) => {
+            println!(
+                "Indent value '{}' is not valid. Defaulting to 4.",
+                indent_str
+            );
+            4
+        }
+    }
+}
+
+fn get_matches() -> ArgMatches<'static> {
+    App::new(crate_name!())
         .version(crate_version!())
         .author(crate_authors!())
         .about(crate_description!())
@@ -28,32 +72,5 @@ fn main() {
                 .required(true)
                 .index(1),
         )
-        .get_matches();
-
-    let indent_str = matches.value_of("indent").unwrap();
-    let indent = match indent_str.parse::<u8>() {
-        Ok(i) => i,
-        Err(_) => {
-            println!(
-                "Indent value '{}' is not valid. Defaulting to 4.",
-                indent_str
-            );
-            4
-        }
-    };
-
-    let options = prettify::options::Options {
-        indent_size: indent,
-        verbose: matches.occurrences_of("verbose") > 0,
-    };
-
-    let input = String::from(matches.value_of("INPUT").unwrap());
-
-    let result = prettify::prettify(&input, &options);
-
-    if result.is_empty() {
-        println!("Input was not in any recognized format.")
-    } else {
-        println!("{}", result);
-    }
+        .get_matches()
 }
